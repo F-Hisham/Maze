dec3259080bb97fe857bcc0958a5dba04568c868
Alexander Mottram <alexanderdmottram@outlook.com>
2022-07-30 20:41:49 +0100

    Addition of solver results and solver stats dataclasses.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from maze.basic_maze import Maze
 from maze.cells import Coords
-from solvers.simple import PseudoDirectionalMazeSolver
+from solvers.simple import PseudoDirectionalMazeSolver, SimpleMazeSolver
 
 
 def main():
@@ -20,8 +20,8 @@ def main():
     start = Coords(x=0, y=0)
     end = Coords(x=9, y=9)
 
-    maze_solver = PseudoDirectionalMazeSolver(maze, start, end, visualize=True)
-    solver_stats = maze_solver.solve()    
+    maze_solver = SimpleMazeSolver(maze, start, end, visualize=True)
+    solver_stats = maze_solver.solver_stats(n=100)    
     print(solver_stats)
     temp = 1
 
--- solvers/base.py
+++ solvers/base.py
@@ -1,9 +1,81 @@
+import random
 import time
 from abc import ABC, abstractmethod
-from typing import Tuple
+from dataclasses import dataclass
+from typing import List, Tuple
 
-from maze.cells import Coords
 from maze.basic_maze import Maze
+from maze.cells import Coords
+from numpy import double
+
+
+@dataclass
+class SolverResult:
+    path: Tuple[Coords]
+    is_valid_path: bool
+    path_length: int
+    solve_time: float
+
+
+@dataclass
+class SolverStats:
+    results_sets: List[SolverResult]
+
+    @property
+    def valid_path_results(self) -> Tuple[bool]:
+        return tuple((res.is_valid_path for res in self.results_sets))
+
+    @property
+    def path_lengths(self) -> Tuple[int]:
+        return tuple((res.path_length for res in self.results_sets))
+
+    @property
+    def solve_times(self) -> Tuple[float]:
+        return tuple((res.solve_time for res in self.results_sets))
+
+    @property
+    def all_paths_valid(self):
+        return all(self.valid_path_results)
+
+    @property
+    def length_stats(self):
+        return {
+            "mean": sum(self.path_lengths) / len(self.path_lengths),
+            "min": min(self.path_lengths),
+            "max": max(self.path_lengths),
+        }
+
+    @property
+    def length_score(self):
+        return sum(self.length_stats.values())
+
+    @property
+    def solve_time_stats(self):
+        return {
+            "mean": sum(self.solve_times) / len(self.solve_times),
+            "min": min(self.solve_times),
+            "max": max(self.solve_times),
+        }
+
+    @property
+    def timer_score(self):
+        return sum(self.solve_time_stats.values()) * 50
+
+    @property
+    def total_score(self):
+        if self.all_paths_valid:
+            return self.length_score + self.timer_score
+        else:
+            return None
+
+    def __repr__(self) -> str:
+        return (
+            f"Solver stats: \n"
+            f"Total score: {self.total_score:.2f} \n"
+            f"All paths valid results: {self.all_paths_valid}\n"
+            f"Path length score ({self.length_score:.2f}): {self.length_stats}\n"
+            f"Solver timing score ({self.timer_score:.2f}): {self.solve_time_stats}"
+        )
 
 
 class BaseMazeSolver(ABC):
@@ -16,15 +88,15 @@ class BaseMazeSolver(ABC):
     ):
         # Do not attempt to modify original maze, it is needed for validation
         self.__original_maze = maze
-        
+
         # Do not modify the start and end coordinates either
         self.start_coords = start_coords
         self.end_coords = end_coords
-        
-        # If you have any plotting, make sure it can be turned on and off using 
+
+        # If you have any plotting, make sure it can be turned on and off using
         # the visualize attribute below
         self.visualize = visualize
-        
+
         # If you have any random number generators, use this for the
         # seed to allow for testing with different random number series
         self.rand_seed = 1
@@ -32,18 +104,17 @@ class BaseMazeSolver(ABC):
     @property
     def original_maze(self):
         return self.__original_maze
-    
+
     @original_maze.setter
     def original_maze(self, value):
         raise RuntimeError("Do not try and change the original maze.")
-    
+
     @abstractmethod
     def solve(self) -> Tuple[Coords]:
         raise NotImplementedError(f"This method must be implemented")
 
-    def solver_stats(self):
-        # TODO -> add a read out of solved information here
-
+    def profile_solve(self) -> SolverResult:
+        """Profile the solve method for a single run."""
         # Turn off visualization and then reapply after solving
         original_vis_setting = self.visualize
         self.visualize = False
@@ -51,18 +122,40 @@ class BaseMazeSolver(ABC):
         start_time = time.time()
         path = self.solve()
         end_time = time.time()
+        run_time = end_time - start_time
 
         self.visualize = original_vis_setting
 
-        stats = {
-            "valid_path": self.validate_path(path),
-            "path": path,
-            "path_length": len(path),
-            "solve_time": end_time - start_time,
-        }
+        solver_result = SolverResult(
+            path=path,
+            is_valid_path=self.validate_path(path),
+            path_length=len(path),
+            solve_time=run_time,
+        )
+        return solver_result
 
-        return stats
+    def solver_stats(self, n=100):
+        seeds = [random.randint(1, 1_000_000) for i in range(n)]
+
+        results_list = []
+
+        for seed in seeds:
+            random.seed(1)
+            solver_results = self.profile_solve()
+            results_list.append(solver_results)
+
+        solver_stats = SolverStats(results_sets=results_list)
+        return solver_stats
 
     def validate_path(self, path: Tuple[Coords]):
-        # TODO -> add path validation test
-        return True
+        is_start = path[0] == self.start_coords
+        is_end = path[-1] == self.end_coords
+
+        path_pairs = tuple(zip(path[:-1], path[1:]))
+        is_all_adjacent = all(
+            [self.original_maze.cell_array.is_adjacent(a, b) for a, b in path_pairs]
+        )
+        is_all_road = all([self.original_maze.is_valid_road(x) for x in path])
+
+        return all([is_start, is_end, is_all_adjacent, is_all_road])
+
